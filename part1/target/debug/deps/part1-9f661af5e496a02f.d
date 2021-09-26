51b10fb34f9112047388f469d99a48e48e14c3d9
sidharthpunathil <sidharthpunathil714@gmail.com>
2021-09-26 19:50:14 +0530

    solution 1 done

---
+++ part1/target/debug/deps/part1-9f661af5e496a02f.d
@@ -0,0 +1,5 @@
+/media/s1dwwth/DEV/rust-intro/part1/target/debug/deps/part1-9f661af5e496a02f: src/main.rs
+
+/media/s1dwwth/DEV/rust-intro/part1/target/debug/deps/part1-9f661af5e496a02f.d: src/main.rs
+
+src/main.rs:
---
+++ part1/target/debug/part1.d
@@ -0,0 +1,1 @@
+/media/s1dwwth/DEV/rust-intro/part1/target/debug/part1: /media/s1dwwth/DEV/rust-intro/part1/src/main.rs
